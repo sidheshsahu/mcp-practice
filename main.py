10187fdd884a0507fabb148944ed8a673dca2aea
Sidhesh Sahu <sidhesh.sahu@somaiya.edu>
2026-06-30 08:40:15 +0530

    Add basic part of server

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mcp-practice!")
+from mcp.server.fastmcp import FastMCP
+
+
+mcp = FastMCP("My First MCP Server")
+
+# Define a tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Adds two numbers."""
+    return a + b
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
